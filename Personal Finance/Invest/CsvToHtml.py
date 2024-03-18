c8dace1f00d8de1555f74af6c07f2606b20a7541
Claire Kraft <79609609+ckraft-bot@users.noreply.github.com>
2024-03-17 21:57:35 -0400

    Update CsvToHtml.py

--- Personal Finance/Invest/CsvToHtml.py
+++ Personal Finance/Invest/CsvToHtml.py
@@ -1,6 +1,6 @@
 import pandas as pd 
 #"C:\Users\ckraft\Desktop\My Experiments\Personal Use\Portfolio\OnBalanceVolume_Ranked.csv"
-csv_file = pd.read_csv(r"C:\Users\ckraft\Desktop\My Experiments\Personal Use\Portfolio\OnBalanceVolume_Ranked.csv")
-csv_file.to_html(r"C:\Users\ckraft\Desktop\My Experiments\Personal Use\Portfolio\OnBalanceVolume_Ranked.html") 
+csv_file = pd.read_csv(r"<path>\OnBalanceVolume_Ranked.csv")
+csv_file.to_html(r"<path>\OnBalanceVolume_Ranked.html") 
 
 html_file = csv_file.to_html()
