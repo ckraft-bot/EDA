fb34c9bc9102a9fec110086f557561298ea79cc8
Claire Kraft <79609609+ckraft-bot@users.noreply.github.com>
2024-03-17 21:53:42 -0400

    Delete Personal Finance/invest directory

--- Personal Finance/invest/CsvToHtml.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd 
-#"C:\Users\ckraft\Desktop\My Experiments\Personal Use\Portfolio\OnBalanceVolume_Ranked.csv"
-csv_file = pd.read_csv(r"C:\Users\ckraft\Desktop\My Experiments\Personal Use\Portfolio\OnBalanceVolume_Ranked.csv")
-csv_file.to_html(r"C:\Users\ckraft\Desktop\My Experiments\Personal Use\Portfolio\OnBalanceVolume_Ranked.html") 
-
-html_file = csv_file.to_html()
